1e6c4f0fe7192dc1f7ed0646e7fb87d9371eeb4f
gmatheushq <matheusemog271@gmail.com>
2024-05-17 14:26:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openWhatsApp() {
+    window.open('https://wa.me/5562992546173', '_blank');
+}
+
+function showServices() {
+    document.getElementById('services').classList.remove('hidden');
+}
+
+// Funções para gerenciar a seleção de serviços e horários serão adicionadas aqui
